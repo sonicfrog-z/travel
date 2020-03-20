b37075dc5fca2e754e68abd83eee3164a81e97f9
sonicfrog-z <haike34@hotmail.com>
2020-03-19 18:50:21 -0700

    Make categories in navigation dynamic along with the category table in data base. Cache category info in redis to prevent frequent request to the database

--- src/main/java/cn/itcast/travel/web/servlet/RoutServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/RoutServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/rout/*")
+@WebServlet("/route/*")
 public class RoutServlet extends BaseServlet {
     private RouteService routService = new RouteServiceImpl();
 
